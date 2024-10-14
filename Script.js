9c75fef9ba80ccc3dd920919c44c0a94d9a7435f
mjdares <84713727+mjdares@users.noreply.github.com>
2024-10-14 18:22:30 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+// Open the CRM Form
+function openForm() {
+	document.getElementById("crmForm").style.display = "block";
+}
+
+// Close the CRM Form
+function closeForm() {
+	document.getElementById("crmForm").style.display = "none";
+}
